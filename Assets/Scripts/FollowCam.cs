21141871e714b7f4f7ee5bbe4c4570b4c9bb716f
Stephenmhuang <Stephenmhuang@live.com>
2017-06-23 18:50:25 -0500

    MissionDemolitionFollowCam
    Mostly complete

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour {
 
 	static public FollowCam S; //followcam singleton
+	public float easing = 0.05f;
+	public Vector2 minXY;
 
 	//feilds set in the Unity Inspector Pane
 	public bool ___________________;
@@ -24,17 +26,27 @@ public class FollowCam : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		//if theres only one line following an if, it doesnt need braces
 		if (poi == null) return; //return if no poi
 
 		//get the position of the poi
 		Vector3 destination = poi.transform.position;
 
+		//limit the x and y to min values
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+
+		//interpolate from the current Camera position toward destination
+		destination = Vector3.Lerp(transform.position, destination, easing);
+
 		//retain a destination.z of camZ
 		destination.z = camZ;
 
 		//set the camera to the desitnation
 		transform.position = destination;
+
+		//set the orthagraphic dize of the camera to keep ground in view
+		this.GetComponent<Camera>().orthographicSize = destination.y +10;
 	}
 }
